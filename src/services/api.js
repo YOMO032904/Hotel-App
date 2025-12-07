3752a9d8300b2991ddc1710fc2c440c1c0347694
Jeroll012504 <jerolldarodomanais@gmail.com>
2025-12-08 00:08:26 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-hm-api.vercel.app/api';
+const API_BASE_URL = 'https://hotel-app-backend-beta.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
